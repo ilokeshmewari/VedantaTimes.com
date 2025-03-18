ea6a3a948550b719967ef45a25a0fdad05429b89
ilokeshmewari <lokeshsinghmewari@gmail.com>
2025-03-18 21:57:22 +0530

    Setup frontend with navbar and footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="w-full p-4 bg-gray-800 text-white text-center">
+        © VedantaTimes.com - All rights reserved.
+      </footer>
+    );
+  }
+  
